4f956af899b088aa7e3044f6459e58c536f105f7
Chris Whittle <chris.mark.whittle@gmail.com>
2022-01-14 13:11:48 -0500

    Python script for analysis of strategy convergence

---
+++ analysis/infoset_convergence.py
@@ -0,0 +1,115 @@
+from math import ceil
+import numpy as np
+from pathlib import Path
+import matplotlib.pyplot as plt
+
+N_CKPT = 100000000
+N_CKPT_NUM = 8
+
+# define keys of interest to look at
+keys = [
+    168 << 3, # AA on BTN
+    1 << 3, # 23o on BTN
+    137 << 3, # Q9s on BTN
+    469134607325,
+    1850794632153519,
+    2513475667670,
+    56460775150,
+    90812056150
+]
+
+# define checkpoint files to read
+INFOSET_PATH = Path('../../data/cfr_data')
+ckpts = list(range(N_CKPT, (N_CKPT_NUM+1)*N_CKPT, N_CKPT))
+infoset_filenames = [
+    f'v2_infosets_500post_ckpt{n}.txt' 
+    for n in ckpts
+]
+
+# bit shifts used for decoding infoset key
+PLAYER_SHIFT = 1
+STREET_SHIFT = 2
+CARD_SHIFT = 13
+ACTION_SHIFT = 3
+
+# given the infoset key, are we facing a bet?
+# used to determine if fold is valid action
+def is_facing_bet(key):
+    key = key >> (PLAYER_SHIFT+STREET_SHIFT+CARD_SHIFT)
+
+    if key == 0:
+        return True
+
+    while key != 0:
+        last_action = key % (2**ACTION_SHIFT)
+        key = key >> ACTION_SHIFT
+    
+    return last_action > 2
+
+keys_set = set(keys)
+probabilities = []
+visits = []
+
+# loop over each infoset checkpoint, getting strategy for each key
+for f in infoset_filenames:
+    probabilities += [ {} ]
+    visits += [ {} ]
+
+    print("Loading", INFOSET_PATH / f, "...")
+    with open(INFOSET_PATH / f) as file:
+        for line in file.readlines():
+            d = line.replace('\n', '').split(' ')
+            try:
+                cur_key = int(d[0])
+                if cur_key in keys_set:
+                    probabilities[-1][cur_key] = [float(x) for x in d[3::2]]
+                    visits[-1][cur_key] = int(d[1])
+
+                    if len(probabilities[-1]) == len(keys):
+                        break
+            except ValueError:
+                continue
+
+N_COLS = 4
+scaled_iters = np.array(ckpts)/1e6
+plt.rcParams.update({
+    'figure.figsize': (12, 8),
+    'axes.xmargin': 0,
+    'font.size': 11,
+    'legend.fontsize': 8,
+    'xtick.labelsize': 7,
+    'ytick.labelsize': 7,
+    'axes.grid.which': 'both'
+})
+plt.figure()
+for i, key in enumerate(keys):
+    num_actions = len(probabilities[0][key])
+    plt.subplot(int(ceil(len(keys)/N_COLS)), min(len(keys), N_COLS), i+1)
+    plt.title(f'{key}')
+    
+    if is_facing_bet(key):
+        actions = ['call', 'fold']
+        actions += [f'raise size #{j+1}' for j in range(num_actions-2)]
+    else:
+        actions = ['check']
+        actions += [f'bet size #{j+1}' for j in range(num_actions-1)]
+
+    for j in range(num_actions):
+        plt.plot(scaled_iters, 
+                [probabilities[f][key][j] 
+                    for f in range(len(infoset_filenames))],
+                '.--', label=actions[j])
+
+
+    plt.xlabel('CFR iterations/1M')
+    plt.ylabel('Action probabilities')
+    plt.legend(loc='best')
+    
+    ax2 = plt.gca().twiny()
+    ax2.set_xticks(scaled_iters)
+    ax2.set_xticklabels(
+        [str(visits[f][key]) for f in range(len(infoset_filenames))],
+        rotation=45)
+    ax2.set_xlabel('Visit counts')
+plt.tight_layout()
+plt.show()
